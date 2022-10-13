b750c15093c3ed8396f2aed9d4289a358e8e26a7
Semen Sova <semen.sova@atos.net>
2022-10-13 22:02:34 +0300

    Добавьте файлы проекта.

---
+++ MainApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Collections.Concurrent;
+using WordCalc;
+
+namespace MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "text.txt";
+            string text = "";
+            using (StreamReader stream = new StreamReader(filePath))
+            {
+                text = stream.ReadToEnd();
+            }
+
+            Type type = typeof(WordCalc.Calc);
+            var a = type.GetMethod("CalculateWordSingle", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Stopwatch calcWatchSingle = Stopwatch.StartNew();
+            ConcurrentDictionary<string, int> map = (ConcurrentDictionary<string, int>)a.Invoke(new Calc(), new object[] { text });
+            calcWatchSingle.Stop();
+
+
+            Stopwatch calcWatchMulti = Stopwatch.StartNew();
+            map = new Calc().CalculateWordMulti(text);
+            calcWatchMulti.Stop();
+
+            Console.WriteLine(calcWatchSingle.ElapsedMilliseconds + "мс потребовалось для операции на одном потоке");
+            Console.WriteLine(calcWatchMulti.ElapsedMilliseconds + "мс потребовалось для операции на нескольких потоках");
+
+            //Сортировка
+            var sort = map.OrderByDescending(x => x.Value);
+
+            //Вывод в файл
+            using (StreamWriter sw = File.CreateText("textOut.txt"))
+            {
+                foreach (var x in sort)
+                {
+                    sw.WriteLine(x.Key + " " + x.Value);
+                }
+            }
+        }
+
+        public static ConcurrentDictionary<string, int> CalculateWord(string text)
+        {
+            ConcurrentDictionary<string, int> map = new ConcurrentDictionary<string, int>();
+            string[] separators = { ",", ".", " ", " ", ";", ":", "?", "!", "\"", "(", ")", "—", "–", "[", "]", "»", "«" };
+
+            string[] masWord = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            Parallel.For(0, masWord.Length - 1, n =>
+            {
+                map.AddOrUpdate(masWord[n], 1, (key, value) => value + 1);
+            });
+
+
+            //string? s = String.Empty;
+            //while ((s = sr.ReadLine()) != null)
+            //{
+            //    string[] mas = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            //    foreach (string ma in mas)
+            //        if (map.ContainsKey(ma)) map[ma]++;
+            //        else map.Add(ma, 1);
+            //}
+
+
+            return map;
+        }
+    }
+}
---
+++ WordCalc/Calc.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Concurrent;
+namespace WordCalc
+{
+    public class Calc
+    {
+        ConcurrentDictionary<string, int> CalculateWordSingle(string text)
+        {
+            ConcurrentDictionary<string, int> map = new ConcurrentDictionary<string, int>();
+            string[] separators = { ",", ".", " ", " ", ";", ":", "?", "!", "\"", "(", ")", "—", "–", "[", "]", "»", "«" };
+
+            string[] masWord = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < masWord.Length; i++)
+            {
+                map.AddOrUpdate(masWord[i], 1, (key, value) => value + 1);
+            }
+            return map;
+        }
+
+        public ConcurrentDictionary<string, int> CalculateWordMulti(string text)
+        {
+            ConcurrentDictionary<string, int> map = new ConcurrentDictionary<string, int>();
+            string[] separators = { ",", ".", " ", " ", ";", ":", "?", "!", "\"", "(", ")", "—", "–", "[", "]", "»", "«" };
+
+            string[] masWord = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            Parallel.For(0, masWord.Length-1, n =>
+            {
+                map.AddOrUpdate(masWord[n], 1, (key, value) => value + 1);
+            });
+
+
+            //string? s = String.Empty;
+            //while ((s = sr.ReadLine()) != null)
+            //{
+            //    string[] mas = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            //    foreach (string ma in mas)
+            //        if (map.ContainsKey(ma)) map[ma]++;
+            //        else map.Add(ma, 1);
+            //}
+
+
+            return map;
+        }
+    }
+}
